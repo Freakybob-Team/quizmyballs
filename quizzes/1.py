83b1a4c59c5606efb3e0688b2baaa5bca77293b5
Wish <100239673+wish13yt@users.noreply.github.com>
2025-03-23 17:46:35 -0500

    add quiz 1 and make it work

--- quizzes/1.py
+++ quizzes/1.py
@@ -1,1 +1,17 @@
+import inquirer
+import subprocess
+print("----------------------")
 print("QuizMyBalls - Am I Freaky?")
+questions = [
+  inquirer.List('freaky',
+                message="Are you freaky?",
+                choices=['Yes', 'No'],
+            ),
+]
+answers = inquirer.prompt(questions)
+if (answers['freaky'] == "Yes"):
+    print("You are Freaky! (100%)")
+else:
+    print("You aren't Freaky! (0%)")
+print("----------------------")
+subprocess.call(["python", "src/quizmyballs.py"], shell=True)
--- src/quizmyballs.py
+++ src/quizmyballs.py
@@ -1,6 +1,14 @@
 import subprocess
-print("Welcome to QuizMyBalls!")
-print("Select the quiz you want to do!")
-print("1. Am I Freaky?")
-print("More quizzes coming soon!")
-choice = input()
+import inquirer
+print("Welcome to QuizMyBalls! (CTRL + C to quit)")
+#print("Select the quiz you want to do!")
+#print("1. Am I Freaky?")
+questions = [
+  inquirer.List('quiz',
+                message="Select the quiz you want to do! More quizzes coming soon.",
+                choices=['1. Am I Freaky?'],
+            ),
+]
+answers = inquirer.prompt(questions)
+if (answers['quiz'] == "1. Am I Freaky?"):
+    subprocess.call(["python", "quizzes/1.py"], shell=True)
