5b8983d91aa31c0d55609cbbf3d08e12600103f5
Wish <100239673+wish13yt@users.noreply.github.com>
2025-03-23 17:32:22 -0500

    begin template

---
+++ quizzes/1.py
@@ -0,0 +1,1 @@
+print("QuizMyBalls - Am I Freaky?")
---
+++ src/quizmyballs.py
@@ -0,0 +1,6 @@
+import subprocess
+print("Welcome to QuizMyBalls!")
+print("Select the quiz you want to do!")
+print("1. Am I Freaky?")
+print("More quizzes coming soon!")
+choice = input()
